3b5676fa6ca95b0a26651408328b1f4bcb3cba96
kisho <nk.110181@icloud.com>
2023-07-10 00:10:04 +0900

    Add interface and enum class for P2P quake

--- app/src/main/java/com/vegcale/architecture/data/network/EarthquakeNetworkDataSource.kt
+++ app/src/main/java/com/vegcale/architecture/data/network/EarthquakeNetworkDataSource.kt
@@ -1,5 +1,6 @@
 package com.vegcale.architecture.data.network
 
+import androidx.annotation.IntRange
 import com.vegcale.architecture.data.model.EarthquakeInfo
 
 /**
@@ -7,4 +8,20 @@ import com.vegcale.architecture.data.model.EarthquakeInfo
  */
 interface EarthquakeApi {
     suspend fun getInfo(format:String, limit: Int, order: String): EarthquakeInfo
+}
+
+enum class P2pquakeCode {
+    ALL,        // 551, 552
+    EARTHQUAKE, // 551
+    TSUNAMI     // 552
+}
+
+interface P2pquakeApi {
+    suspend fun getHistory(
+        codes: P2pquakeCode,
+        @IntRange(from = 1, to = 100) limit: Int,
+        @IntRange(from = 0, to = 100) offset: Int
+    ): Unit
+
+    suspend fun getInfo(id: Int): Unit
 }
